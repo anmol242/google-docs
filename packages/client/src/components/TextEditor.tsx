88775c39e27a73ce9c0fe617ff0c0b6c3da0264c
Anmol Mahajan <anmolmahajan242@gmail.com>
2024-02-04 21:36:25 +0530

    chore: add more options to toolbar of document

--- packages/client/src/components/TextEditor.tsx
+++ packages/client/src/components/TextEditor.tsx
@@ -3,6 +3,18 @@ import Quill from "quill";
 import "quill/dist/quill.snow.css";
 import "./textEditor.css";
 
+const TOOLBAR_OPTIONS = [
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+  [{ font: [] }],
+  [{ list: "ordered" }, { list: "bullet" }],
+  ["bold", "italic", "underline"],
+  [{ color: [] }, { background: {} }],
+  [{ script: "sub" }, { script: "super" }],
+  [{ align: [] }],
+  ["image", "blockquote", "code-block"],
+  ["clean"],
+];
+
 export default function TextEditor() {
   const containerRef = useCallback((wrapper: HTMLDivElement) => {
     if (!wrapper) {
@@ -13,7 +25,7 @@ export default function TextEditor() {
 
     wrapper.appendChild(editor);
 
-    new Quill(editor, { theme: "snow" });
+    new Quill(editor, { theme: "snow", modules: { toolbar: TOOLBAR_OPTIONS } });
 
     return () => {
       wrapper.innerHTML = "";
